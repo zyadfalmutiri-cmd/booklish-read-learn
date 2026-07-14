185e5d28078fe10c1f26325dad79f9b3194c7184
zyadfalmutiri-cmd <zyadf.almutiri@gmail.com>
2026-07-14 14:38:31 +0300

    Update step.tsx

--- src/routes/step.tsx
+++ src/routes/step.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
-import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import {
   GraduationCap,
