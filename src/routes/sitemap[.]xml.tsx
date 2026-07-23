671a70fc694481473a14e6454e886e65117b8c9d
zyadfalmutiri-cmd <zyadf.almutiri@gmail.com>
2026-07-23 22:09:23 +0300

    Create sitemap[.]xml.tsx

---
+++ src/routes/sitemap[.]xml.tsx
@@ -0,0 +1,48 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { blogPosts } from '~/data/blog-posts'
+
+const SITE_URL = 'https://booklish.app'
+
+const staticRoutes = [
+  '',
+  '/about',
+  '/contact',
+  '/faq',
+  '/blog',
+  '/step',
+]
+
+export const Route = createFileRoute('/sitemap.xml')({
+  loader: () => {
+    const staticUrls = staticRoutes
+      .map(
+        (path) => `
+  <url>
+    <loc>${SITE_URL}${path}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>${path === '' ? '1.0' : '0.7'}</priority>
+  </url>`,
+      )
+      .join('')
+
+    const blogUrls = blogPosts
+      .map(
+        (post) => `
+  <url>
+    <loc>${SITE_URL}/blog/${post.slug}</loc>
+    <lastmod>${post.date}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>`,
+      )
+      .join('')
+
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${staticUrls}${blogUrls}
+</urlset>`
+
+    throw new Response(xml, {
+      headers: { 'Content-Type': 'application/xml' },
+    })
+  },
+})
